5517dc6ecb9f5316e91d7dccccc1d51c352fe2ca
Danijel Koržinek <danijel@pja.edu.pl>
2017-10-21 16:23:14 +0200

    Chagned from lecture in 1 file to many files for easier management.

--- convert.sh
+++ convert.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
-jupyter nbconvert Wykład.ipynb --to slides
+jupyter nbconvert 1_Wprowadzenie.ipynb --to slides
+jupyter nbconvert 2_Sygnały.ipynb --to slides
+jupyter nbconvert 3_Systemy.ipynb --to slides
+jupyter nbconvert 4_Fourier.ipynb --to slides
 
-sed "s|/theme/simple.css|/theme/blood.css|g" -i Wykład.slides.html
+sed "s|/theme/simple.css|/theme/blood.css|g" -i 1_Wprowadzenie.slides.html
+sed "s|/theme/simple.css|/theme/blood.css|g" -i 2_Sygnały.slides.html
+sed "s|/theme/simple.css|/theme/blood.css|g" -i 3_Systemy.slides.html
+sed "s|/theme/simple.css|/theme/blood.css|g" -i 4_Fourier.slides.html
