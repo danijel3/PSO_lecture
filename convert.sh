ae3493c0506f79527821bda8f4d97d4633b34de4
Danijel Koržinek <danijel@pja.edu.pl>
2017-11-05 02:13:16 +0100

    Started sampling. Fixed FT a bit.

--- convert.sh
+++ convert.sh
@@ -4,8 +4,10 @@ jupyter nbconvert 1_Wprowadzenie.ipynb --to slides
 jupyter nbconvert 2_Sygnały.ipynb --to slides
 jupyter nbconvert 3_Systemy.ipynb --to slides
 jupyter nbconvert 4_Fourier.ipynb --to slides
+jupyter nbconvert 5_Próbkowanie.ipynb --to slides
 
 sed "s|/theme/simple.css|/theme/blood.css|g" -i 1_Wprowadzenie.slides.html
 sed "s|/theme/simple.css|/theme/blood.css|g" -i 2_Sygnały.slides.html
 sed "s|/theme/simple.css|/theme/blood.css|g" -i 3_Systemy.slides.html
 sed "s|/theme/simple.css|/theme/blood.css|g" -i 4_Fourier.slides.html
+sed "s|/theme/simple.css|/theme/blood.css|g" -i 5_Próbkowanie.slides.html
