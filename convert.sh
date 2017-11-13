94dd940c2291bbdb52608e1e1f5f9203489fc572
Danijel Koržinek <danijel@pja.edu.pl>
2017-11-13 23:35:00 +0100

    Changed theme

--- convert.sh
+++ convert.sh
@@ -6,10 +6,3 @@ jupyter nbconvert 3_Systemy.ipynb --to slides
 jupyter nbconvert 4_Fourier.ipynb --to slides
 jupyter nbconvert 5_Próbkowanie.ipynb --to slides
 jupyter nbconvert 6_DFT.ipynb --to slides
-
-sed "s|/theme/simple.css|/theme/blood.css|g" -i 1_Wprowadzenie.slides.html
-sed "s|/theme/simple.css|/theme/blood.css|g" -i 2_Sygnały.slides.html
-sed "s|/theme/simple.css|/theme/blood.css|g" -i 3_Systemy.slides.html
-sed "s|/theme/simple.css|/theme/blood.css|g" -i 4_Fourier.slides.html
-sed "s|/theme/simple.css|/theme/blood.css|g" -i 5_Próbkowanie.slides.html
-sed "s|/theme/simple.css|/theme/blood.css|g" -i 6_DFT.html
