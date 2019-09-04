60528092c70aa404beb54b9162fbfa736218c6fd
Danijel Korzinek <danijel.korzinek@iqor.pl>
2019-09-04 18:57:25 +0200

    Restructruing.

--- convert.sh
+++ convert.sh
@@ -1,16 +1,5 @@
 #!/bin/sh
 
-jupyter nbconvert 1_Wprowadzenie.ipynb --to slides
-jupyter nbconvert 2_Sygnały.ipynb --to slides
-jupyter nbconvert 3_Systemy.ipynb --to slides
-jupyter nbconvert 4_Fourier.ipynb --to slides
-jupyter nbconvert 5_Próbkowanie.ipynb --to slides
-jupyter nbconvert 6_DFT.ipynb --to slides
-jupyter nbconvert 7_filtry.ipynb --to slides
-jupyter nbconvert 8_obrazy.ipynb --to slides
-jupyter nbconvert 9_obrazy_punkt.ipynb --to slides
-jupyter nbconvert 10_obrazy_transformaty.ipynb --to slides
-jupyter nbconvert 11_obrazy_cechy.ipynb --to slides
-jupyter nbconvert 12_ml.ipynb --to slides
-jupyter nbconvert 13_Film.ipynb --to slides
-jupyter nbconvert Podsumowanie.ipynb --to slides
+for f in wykład/*.ipynb ; do
+	jupyter nbconvert $f --to slides
+done
