95c48997d13da13a8c12a426836366200fa3b98c
Danijel Koržinek <danijel@pja.edu.pl>
2019-10-01 22:20:12 +0200

    minor fix

--- convert.sh
+++ convert.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 for f in wykład/*.ipynb ; do
-  if [ $f -nt ${f%ipynb}.slides.html ] ; then
+  if [ $f -nt ${f%ipynb}slides.html ] ; then
 	  jupyter nbconvert $f --to slides
 	else
 	  echo $f did not change...
