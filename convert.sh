fdd659bfe77b89232900f10862f8ea699d0be2c3
danijel <4k3qNtz9EaFBfeS>
2021-10-13 11:17:34 +0200

    Recreated the slides to the new version

--- convert.sh
+++ convert.sh
@@ -3,6 +3,7 @@
 for f in wykład/*.ipynb ; do
   if [[ ! -e ${f%ipynb}slides.html || $f -nt ${f%ipynb}slides.html ]] ; then
 	  jupyter nbconvert $f --to slides
+	  sed -i 's/automatic: true/automatic: false/' ${f%ipynb}slides.html
 	else
 	  echo $f did not change...
   fi
